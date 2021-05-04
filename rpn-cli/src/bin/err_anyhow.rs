b2ca32c041f2b531545c1b4f1c32b6d66eb41834
nixiesquid <audu817@gmail.com>
2021-05-05 06:43:01 +0900

    update around anyhow error handling

--- rpn-cli/src/bin/err_anyhow.rs
+++ rpn-cli/src/bin/err_anyhow.rs
@@ -1,10 +1,15 @@
-use anyhow::{Context, Result};
+use anyhow::{bail, ensure, Context, Result};
 
 fn get_int_from_file() -> Result<i32> {
     let path = "number.txt";
 
-    let num_str = std::fs::read_to_string(path)
-        .with_context(|| format!("failed to read string from {}", path))?;
+    let num_str = std::fs::read_to_string(path)?;
+
+    if num_str.len() >= 10 {
+        bail!("it may be too large number(number length should be less than 10)");
+    }
+
+    ensure!(num_str.starts_with("1"), "first digit should be 1");
 
     num_str
         .trim()
